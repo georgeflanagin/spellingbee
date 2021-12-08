ee5d34c606cdbc041153628836e750911b093d9b
George Flanagin <gflanagin@richmond.edu>
2021-12-08 09:23:49 -0500

    Initial version.

---
+++ bee.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+import typing
+from   typing import *
+
+min_py = (3, 8)
+
+###
+# Standard imports, starting with os and sys
+###
+import os
+import sys
+if sys.version_info < min_py:
+    print(f"This program requires Python {min_py[0]}.{min_py[1]}, or higher.")
+    sys.exit(os.EX_SOFTWARE)
+
+###
+# Other standard distro imports
+###
+import argparse
+import platform
+import re
+
+###
+# From hpclib
+###
+
+###
+# Credits
+###
+__author__ = 'George Flanagin'
+__copyright__ = 'Copyright 2021'
+__credits__ = None
+__version__ = 0.1
+__maintainer__ = 'George Flanagin'
+__email__ = ['me@georgeflanagin.com', 'gflanagin@richmond.edu']
+__status__ = 'in progress'
+__license__ = 'MIT'
+
+this_os = platform.system()
+if this_os == 'Linux':
+    default_word_list = '/usr/share/dict/linux.words'
+elif this_os == 'Darwin':
+    default_word_list = '/usr/share/dict/words'
+else:
+    default_word_list = './words'
+
+
+def bee_main(myargs:argparse.Namespace) -> int:
+    with open(myargs.dictionary) as f:
+        words = tuple(_.lower() for _ in f.read().split('\n') if len(_) > 3)
+
+    if myargs.middle:
+        r_letter = myargs.middle
+        letters = myargs.letters
+    else:
+        r_letter = myargs.letters[0]
+        letters = myargs.letters[1:]
+    plenum = r_letter+letters
+
+    expression = f"[{letters}]*{r_letter}[{plenum}]*"
+    c_expression = re.compile(expression)
+
+    results = list(_ for _ in words if c_expression.fullmatch(_))
+        
+    print(results)
+
+    return os.EX_OK
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser(prog="bee", 
+        description="What bee does, bee does best.")
+
+    parser.add_argument('-d', '--dictionary', type=str, default=default_word_list,
+        help="Name of the dictionary file.")
+    parser.add_argument('-l', '--letters', type=str, required=True,
+        help="Letters to use, either six letters, or seven with the required letter first.")
+    parser.add_argument('-m', '--middle', type=str, required=True,
+        help="Output file name")
+    parser.add_argument('-v', '--verbose', type=int, default=0, choices=(0, 1, 2, 3),
+        help="Be chatty about what is taking place -- on a scale of 0 to 3")
+
+    myargs = parser.parse_args()
+    myargs.verbose and linuxutils.dump_cmdline(myargs)
+
+    try:
+        vm_callable = "{}_main".format(os.path.basename(__file__)[:-3])
+        sys.exit(globals()[vm_callable](myargs))
+
+    except Exception as e:
+        print(f"Unhandled exception {e}")
+
+
